66a91fcc1ea54a1c2ea4fd171b7e68c91d5e3849
Krzysztof Zadka <kzadka89@gmail.com>
2021-02-22 19:49:18 +0100

    Add first method

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,7 +1,15 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        System.out.println("X O X");
+        System.out.println("O X O");
+        System.out.println("X X O");
+
+        Scanner scanner = new Scanner(System.in);
+
     }
 }

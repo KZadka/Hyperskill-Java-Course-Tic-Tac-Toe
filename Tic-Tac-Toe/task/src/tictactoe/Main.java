e7c0008a0eeda2959a3f267eb883432ce2093732
Krzysztof Zadka <kzadka89@gmail.com>
2021-03-26 17:24:07 +0100

    Before refactoring of working code

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter cells: ");
         String userInput = scanner.nextLine();
         Character[][] field = new Character[3][3];
+
         int counter = 0;
         for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field[i].length; j++) {
@@ -19,6 +21,49 @@ public class Main {
         System.out.println(secondLine(userInput));
         System.out.println(thirdLine(userInput));
         lineOfDashes();
+        String result = "";
+        boolean xWins = false;
+        boolean oWins = false;
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+               // int rows = field[0][j] + field[1][j] + field[2][j];
+               // int columns = field
+                if (field[0][0] + field[0][1] + field[0][2] == 264 ||//poziomy
+                    field[1][0] + field[1][1] + field[1][2] == 264 ||//poziomy
+                    field[2][0] + field[2][1] + field[2][2] == 264 ||//poziomy
+                    field[0][0] + field[1][0] + field[2][0] == 264 ||//piony
+                    field[0][1] + field[1][1] + field[2][1] == 264 ||//piony
+                    field[0][2] + field[1][2] + field[2][2] == 264 ||//piony
+                    field[0][0] + field[1][1] + field[2][2] == 264 ||//skosy
+                    field[0][2] + field[1][1] + field[2][0] == 264) {//skosy
+                    xWins = true;
+                }
+                if (field[0][0] + field[0][1] + field[0][2] == 237 ||//poziomy
+                    field[1][0] + field[1][1] + field[1][2] == 237 ||//poziomy
+                    field[2][0] + field[2][1] + field[2][2] == 237 ||//poziomy
+                    field[0][0] + field[1][0] + field[2][0] == 237 ||//piony
+                    field[0][1] + field[1][1] + field[2][1] == 237 ||//piony
+                    field[0][2] + field[1][2] + field[2][2] == 237 ||//piony
+                    field[0][0] + field[1][1] + field[2][2] == 237 ||//skosy \
+                    field[0][2] + field[1][1] + field[2][0] == 237) {//skosy /
+                    oWins = true;
+                }
+            }
+        }
+        if ((oWins && xWins) || differenceCount(field)) {
+            result = "Impossible";
+        } else if (oWins) {
+            result = "O wins";
+        } else if (xWins) {
+            result = "X wins";
+        } else if (!(xWins && oWins) && isFull(field)) {
+            result = "Draw";
+        } else if (!(xWins && oWins)) {
+            result = "Game not finished";
+        }
+        System.out.println(result);
+
+
     }
     private static void lineOfDashes() {
         System.out.println("---------");
@@ -35,4 +80,35 @@ public class Main {
         String thirdText = userInput.substring(6, 9);
         return "| " + thirdText.charAt(0) + " " + thirdText.charAt(1) + " " + thirdText.charAt(2)  + " |";
     }
+
+    private static Boolean isFull(Character[][] field) {
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if (field[i][j] == '_') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    private static boolean differenceCount(Character[][] matrix) {
+        int x = 0;
+        int o = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 'X')  {
+                    x++;
+                } else if (matrix[i][j] == 'O') {
+                    o++;
+                }
+            }
+        }
+        if (Math.abs(x - o ) >= 2 || Math.abs(o - x) >= 2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+
+
